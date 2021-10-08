1f52c165023127062fdc48921f9f2aff29d87545
Amira Ezzeldin <aezz4007@gmail.com>
2021-10-08 19:49:37 +0200

    Working initial project

---
+++ webpack.prod.js
@@ -0,0 +1,39 @@
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const cssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    devtool: 'source-map',  
+    optimization: {
+        minimizer: [new TerserPlugin({}), new cssMinimizerWebpackPlugin({})],
+        },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    }, 
+    module: {
+        rules: [
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                },
+                {
+                    test: /\.scss$/,
+                    use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+                }
+        ]
+},
+plugins: [
+    new HtmlWebPackPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" })
+]
+}
